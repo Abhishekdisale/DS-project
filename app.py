09e711ef7c1bfd0f30d8f665d12fa71d25c7c11e
Abhishekdisale <160887661+Abhishekdisale@users.noreply.github.com>
2026-03-02 14:12:46 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -40,29 +40,4 @@ if st.button("Predict"):
         st.success(f"Predicted Condition: {prediction}")
 
     else:
-        st.warning("Please enter both drug name and review")import streamlit as st
-import joblib
-
-# Load model
-model = joblib.load("drug_condition_model.pkl")
-
-st.title("Drug Review Disease Predictor")
-
-st.write("Enter drug name and review to predict the condition.")
-
-drug = st.text_input("Drug Name")
-
-review = st.text_area("Enter Review")
-
-if st.button("Predict"):
-
-    if drug and review:
-
-        text = drug + " " + review
-
-        prediction = model.predict([text])[0]
-
-        st.success(f"Predicted Condition: {prediction}")
-
-    else:
         st.warning("Please enter both drug name and review")
